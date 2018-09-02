4bb737a3eeca1423b6783d30450e833d36ed5801
Cody Bumgardner <cody@uky.edu>
2018-09-02 17:29:04 -0400

    fix version

--- build.sh
+++ build.sh
@@ -11,4 +11,3 @@ cd ..
 cd controller
 mvn clean package bundle:bundle -U
 cp target/controller-1.0-SNAPSHOT.jar ../agent/src/main/resources/
-cp target/controller-1.0-SNAPSHOT.jar ../felix-framework-5.6.10/bundle/
